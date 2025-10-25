815de0694cef4668f37cbde07d21ef7ab91321e4
sohhhyeeun <syeankm12076@gmail.com>
2025-10-25 15:06:55 +0900

    feat: 시도할 횟수 유효성 검증

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,15 +5,27 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String carNamesStr = InputView.readCarNames();
-        String attemptCountStr = InputView.readAttemptCount();
-
-        String[] carNames = Division.divideCarNames(carNamesStr);
+        String carNamesStr;
+        String[] carNames;
+        String attemptCountStr;
 
         try {
+            carNamesStr = InputView.readCarNames();
+            carNames = Division.divideCarNames(carNamesStr);
+
             Validation.validateCarNames(carNames);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+            return;
+        }
+
+        try {
+            attemptCountStr = InputView.readAttemptCount();
+
+            Validation.validateAttemptCount(attemptCountStr);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
         }
     }
 }
--- src/main/java/racingcar/Validation.java
+++ src/main/java/racingcar/Validation.java
@@ -20,4 +20,25 @@ public class Validation {
             throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
         }
     }
+
+    public static void validateAttemptCount(String attemptCountStr) {
+        validateIsNumeric(attemptCountStr);
+        validateIsPositive(attemptCountStr);
+    }
+
+    private static void validateIsNumeric(String attemptCountStr) {
+        try {
+            Integer.parseInt(attemptCountStr);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도할 횟수는 숫자로만 입력해 주세요.");
+        }
+    }
+
+    private static void validateIsPositive(String attemptCountStr) {
+        int attemptCount = Integer.parseInt(attemptCountStr);
+
+        if (attemptCount < 1) {
+            throw new IllegalArgumentException("시도할 횟수는 양의 정수만 가능합니다.");
+        }
+    }
 }
